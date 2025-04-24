a5f7db6d999e8f64e303717e976d5b855fec86fa
AbdullahAlTalaq <99118138+AbdullahAlTalaq@users.noreply.github.com>
2025-04-24 09:41:47 +0300

    Update AiAgentOllama.py

--- AiAgentOllama.py
+++ AiAgentOllama.py
@@ -1,64 +1,94 @@
-import ollama
-from msgraph import GraphServiceClient
-from msgraph.generated.users.item.send_mail.send_mail_post_request_body import SendMailPostRequestBody
-from msgraph.generated.models.message import Message
-from msgraph.generated.models.item_body import ItemBody
-from msgraph.generated.models.body_type import BodyType
-from msgraph.generated.models.recipient import Recipient
-from msgraph.generated.models.email_address import EmailAddress
-from azure.identity import DeviceCodeCredential
-import asyncio
-import re
+from agno.agent import Agent
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.tools.yfinance import YFinanceTools
+from agno.models.ollama import Ollama
+from agno.tools import tool
+from agno.team.team import Team
+import random
+from rich.pretty import pprint
+from utils.GmailEmailSender import GmailEmailSender 
+from agno.embedder.ollama import OllamaEmbedder
+from agno.vectordb.lancedb import LanceDb, SearchType
+from agno.knowledge.pdf import PDFKnowledgeBase, PDFReader
 
-scope = ["User.Read", "Mail.Send", "Mail.Read"]
-tenant_id ="2c86bbfc-8d04-41ff-a83a-942f075e0f60"
-client_id = "8f6af9ce-445a-47b3-9134-e21738881000"
-credential = DeviceCodeCredential(tenant_id=tenant_id,client_id=client_id)
-
-graph_client = GraphServiceClient(credential, scope)
-
-def generate_email_prompt(message):
-  
-  output = ollama.generate(
-  model="deepseek-coder-v2",
-  prompt=f"""Call Ollama with the Llama 3.2 model"""
-         """You are an email assistant that helps users interact with their Microsoft Outlook.
-please write en email using this message {message}
-Only return the message.""",
-  stream=False,
+knowledge_base = PDFKnowledgeBase(
+        path="Files\Career Co.pdf",
+        vector_db=LanceDb(
+            uri="tmp/lancedb",
+            table_name="payslip",
+            search_type=SearchType.hybrid,
+            embedder=OllamaEmbedder(id="openhermes"),
+          ),
+    reader=PDFReader(chunk=True),
 )
-  
-  return output['response']
 
-def extract_email(prompt):
-   email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-   emails = re.findall(email_pattern, prompt)
+@tool(show_result=True, stop_after_tool_call=True)
+def Send_Email(receiver:str,email_subject:str,message_body:str ) -> str:
+    """This tool is for sending email. receiver: This argument contains the recipient's email address, message_body: This argument contains the body of the message, email_subject: This argument contains the subject line of the message. """
+    
+    try:
+
+        gmail_sender = GmailEmailSender(user_id='user3')  # Unique ID per user
+        gmail_sender.send_email(
+        to=receiver,
+        subject= email_subject,
+        body=message_body
+        )
+        return "Sent successfully"
+    except Exception as e:
+        
+        return "Send failed"
 
 
 
-async def send_email():
-    x =input("Enter your prompt: ")
-    emails = extract_email(x)
-    message_email = generate_email_prompt(x)
-    request_body = SendMailPostRequestBody(
-	message = Message(
-		subject = "Test",
-		body = ItemBody(
-			content_type = BodyType.Text,
-			content = message_email,
-		),
-		to_recipients = [
-			Recipient(
-				email_address = EmailAddress(
-					address = emails,
-				),
-			),
-		],
-	),
-	save_to_sent_items = False,
-)    
-    await graph_client.me.send_mail.post(request_body)
- 
 
-if __name__ == '__main__':
-    asyncio.run(send_email()) 
+Send_Email_agent = Agent(
+    name="Send Email agent",
+    role="write and Send an Email to a specific person",
+    knowledge=knowledge_base,
+    model=Ollama(id="qwq"),
+    tools=[Send_Email,DuckDuckGoTools()],
+    instructions=["You are an Agent who writes the email using the knowledge base. If you need information that is not there, use the DuckDuckGoTools tool.",
+                  "Use the knowledge base and if that's not enough for you, use DuckDuckGoTools too.",
+                  "Write a professional email, and prepare the receiver (email address), email_subject (subject line), and message_body (body of the message).",
+                  "After writing the email, use the Send_Email tool to send the email.",
+                  "Implement things, use tools, and do not overthink and explain"],
+    markdown=True,
+)
+if Send_Email_agent.knowledge is not None:
+    Send_Email_agent.knowledge.load()
+agent_team = Team(
+    name="Team leader",
+    mode="coordinate",
+    model=Ollama(id="qwq"),  # You can use a different model for the team leader agent
+    members=[Send_Email_agent],
+    instructions=["""Always use the info that is available in the tools only.
+    If you receive 'Sent successfully' or 'Send failed' from the tool, stop sending another email.
+    To send an email, follow these steps:
+       1. Call the Send_Email_agent function.
+    If you receive 'Sent successfully':
+       1. Print that the sending was successful.
+       2. Print the entire message (receiver, subject, body).
+    If you receive 'Send failed':
+       1. Print that the sending failed.
+       2. Print the message 'Not sent'.""",
+       "Implement things, use tools, and do not overthink and explain"],
+    show_tool_calls=True,  # Uncomment to see tool calls in the response
+    markdown=True,
+)
+
+# Give the team a task
+agent_team.print_response("Write about Co-Ownership based on knowledge base, And write to me about the new iPhone 16e from Apple.When you done, send the information to Yahya(y75910@gmail.com) via email. ", stream=True)
+
+# if __name__ == "__main__":
+
+#     agent_team.print_response("Who is the best singer in my country KSA song ?")
+#     agent_team.print_response("What is the weather like in my country?")
+
+
+    # Print all messages in this session
+    #messages_in_session = agent_team.get_messages_for_session()
+    #pprint(messages_in_session)
+
+
+
