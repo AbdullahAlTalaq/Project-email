0f26c388e2a68f5163602b0c40c6cfa3d02eb509
AbdullahAlTalaq <99118138+AbdullahAlTalaq@users.noreply.github.com>
2025-04-24 15:02:21 +0300

    Create AiOpenSKD.py

---
+++ AiOpenSKD.py
@@ -0,0 +1,140 @@
+'''
+pip install openai-agents
+Hello world example
+
+from agents import Agent, Runner
+
+agent = Agent(name="Assistant", instructions="You are a helpful assistant")
+
+result = Runner.run_sync(agent, "Write a haiku about recursion in programming.")
+print(result.final_output)
+
+'''
+'''
+Create your first agent
+
+from agents import Agent
+
+agent = Agent(
+    name="Math Tutor",
+    instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
+)
+'''
+
+
+
+
+'''
+
+from agents import Runner,Agent
+
+#Handoff used 
+
+
+history_tutor_agent = Agent(
+    name="History Tutor",
+    handoff_description="Specialist agent for historical questions",
+    instructions="You provide assistance with historical queries. Explain important events and context clearly.",
+)
+
+math_tutor_agent = Agent(
+    name="Math Tutor",
+    handoff_description="Specialist agent for math questions",
+    instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
+)
+triage_agent = Agent(
+    name="Triage Agent",
+    instructions="You determine which agent to use based on the user's homework question",
+    handoffs=[history_tutor_agent, math_tutor_agent]
+)
+
+async def main():
+    result = await Runner.run(triage_agent, "What is the capital of France?")
+    print(result.final_output)
+
+'''
+
+'''
+#Add a guardrail
+from agents import GuardrailFunctionOutput, Agent, Runner
+from pydantic import BaseModel
+
+class HomeworkOutput(BaseModel):
+    is_homework: bool
+    reasoning: str
+
+
+guardrail_agent = Agent(
+    name="Guardrail check",
+    instructions="Check if the user is asking about homework.",
+    #passed class parameters to output_type 
+    output_type=HomeworkOutput,
+)
+
+async def homework_guardrail(ctx, agent, input_data):
+    result = await Runner.run(guardrail_agent, input_data, context=ctx.context)
+    final_output = result.final_output_as(HomeworkOutput)
+    return GuardrailFunctionOutput(
+        output_info=final_output,
+        tripwire_triggered=not final_output.is_homework,
+    )
+
+'''
+'''
+from agents import Runner,Agent
+
+#Handoff used 
+class HomeworkOutput(BaseModel):
+    is_homework: bool
+    reasoning: str
+
+history_tutor_agent = Agent(
+    name="History Tutor",
+    handoff_description="Specialist agent for historical questions",
+    instructions="You provide assistance with historical queries. Explain important events and context clearly.",
+)
+
+math_tutor_agent = Agent(
+    name="Math Tutor",
+    handoff_description="Specialist agent for math questions",
+    instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
+)
+triage_agent = Agent(
+    name="Triage Agent",
+    instructions="You determine which agent to use based on the user's homework question",
+    handoffs=[history_tutor_agent, math_tutor_agent]
+)
+
+
+
+
+#guardrails
+async def homework_guardrail(ctx, agent, input_data):
+    result = await Runner.run(guardrail_agent, input_data, context=ctx.context)
+    final_output = result.final_output_as(HomeworkOutput)
+    return GuardrailFunctionOutput(
+        output_info=final_output,
+        tripwire_triggered=not final_output.is_homework,
+    )
+
+
+#handover
+triage_agent = Agent(
+    name="Triage Agent",
+    instructions="You determine which agent to use based on the user's homework question",
+    handoffs=[history_tutor_agent, math_tutor_agent],
+    input_guardrails=[
+        InputGuardrail(guardrail_function=homework_guardrail),
+    ],
+)
+
+async def main():
+    result = await Runner.run(triage_agent, "who was the first president of the united states?")
+    print(result.final_output)
+
+    result = await Runner.run(triage_agent, "what is life")
+    print(result.final_output)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+'''
